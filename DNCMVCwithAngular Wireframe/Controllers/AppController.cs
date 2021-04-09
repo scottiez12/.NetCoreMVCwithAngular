549d24fb3671038404b93bc3cc24c3264d0edafa
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-04-09 14:44:13 -0400

    MVC part 1

---
+++ DNCMVCwithAngular Wireframe/Controllers/AppController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DNCMVCwithAngular_Wireframe.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            //throw new InvalidProgramException("Bad things happen to good developers..");
+            return View();
+        }
+
+
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About";
+
+            return View();
+        }
+
+    }
+}
--- DNCMVCwithAngular Wireframe/Startup.cs
+++ DNCMVCwithAngular Wireframe/Startup.cs
@@ -16,31 +16,39 @@ namespace DNCMVCwithAngular_Wireframe
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //configure what services the server needs for its' middleware
+            services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //from course
-            app.UseDefaultFiles();
+            //use the IWebHostEnvironment variable above to decide if we're in dev or production or staging or whatever
+            if (env.IsDevelopment())
+            {
+                //this shows detailed info on errors
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            //this first ensures that all our js/css/img/whatever files get loaded first
             app.UseStaticFiles();
 
+            //allows us to route individual calls that come into the server to individual pieces of code
+            app.UseRouting();
 
+            //by using endpoints, we can tell the routes where to go
+            app.UseEndpoints(cfg =>
+            {
+                //so this sets up the pattern and default behavior
+                cfg.MapControllerRoute("Default",
+                    //this below means that it will go by controller => the action/method from the controller => optionally an ID in the URL
+                    "/{controller}/{action}/{id?}",
+                    //this anon object specifys default behavior... so if the request is just /{controller}, then go to the default instead of crashing, or whatever
+                    new { controller = "App", action = "Index" });
+            });
 
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-
-            //app.UseRouting();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        await context.Response.WriteAsync("Hello World!");
-            //    });
-            //});
         }
     }
 }
