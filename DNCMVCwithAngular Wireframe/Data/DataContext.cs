67daa8752514118abcfddcf57972dc131eabb123
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-04-14 10:26:56 -0400

    Sync

--- DNCMVCwithAngular Wireframe/Data/DataContext.cs
+++ DNCMVCwithAngular Wireframe/Data/DataContext.cs
@@ -1,5 +1,6 @@
 ﻿using DutchTreat.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,14 @@ namespace DNCMVCwithAngular_Wireframe.Data
 {
     public class DataContext : DbContext
     {
+        private readonly IConfiguration _config;
+        //use ctor to add IConfig and generate the field
+        //then add it to the "UseSqlServer()" below
+        public DataContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
 
@@ -16,7 +25,9 @@ namespace DNCMVCwithAngular_Wireframe.Data
         {
             base.OnConfiguring(optionsBuilder);
 
-            optionsBuilder.UseSqlServer();
+            //you can do it this way, or _config.GetConnectionString()
+            //but this way is explicitly calling it from the json file
+            optionsBuilder.UseSqlServer(_config["ConnectionStrings:DataContextDb"]);
         }
     }
 }
